3d0dccfbb9402babd871331ac039721a1dfad097
GaianiMagali <magaliantonella.gaiani@gmail.com>
2019-11-17 21:43:52 -0300

    commit inicial

---
+++ src/com/company/Java/Shape.java
@@ -0,0 +1,5 @@
+package com.company.Java;
+
+public interface Shape {
+    void draw();
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+import com.company.Java.ShapeMaker;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+}
